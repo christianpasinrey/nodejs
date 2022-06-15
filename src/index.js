31ff8a4bec0a920439a3fdf0944ca1e7e21da05f
christianpasinrey <70205361+christianpasinrey@users.noreply.github.com>
2022-06-15 23:41:11 +0200

    Public path working

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import { dirname, join } from 'path'; //module for working with paths
 import { fileURLToPath } from 'url'; //module for working with paths-url
 import router from './routes/router.js'; //module for working with paths-url
 import expressEjsExtend from 'express-ejs-extend';
+import path  from 'path';
 const app = express(); //init app
-
+const __publicDir = path.resolve("./public"); //get path to public dir
 const __dirname = dirname(fileURLToPath( //get path of src folder project
     import.meta.url));
 console.log(__dirname);
 app.engine('ejs', expressEjsExtend); // add this line
 app.set('view engine', 'ejs'); //set engine of views; previously npm i ejs; not imported cause node supports it by default
 app.set('views', join(__dirname, 'views')); //set path of views; dirname concat views with join method
-
+app.use(express.static(__publicDir)); //set path of public folder; dirname concat public with join method
 /* app.use("/public", express.static("static")); */
 app.use(router); //use router
 
